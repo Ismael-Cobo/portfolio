a991d53255d66a01d50e64f47eb9123a5a7a367d
Ismael Cobo <midismael@gmail.com>
2022-04-04 14:53:57 +0200

    text fix

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,7 +4,7 @@ export const About = () => {
   return (
     <div id='about' className='white_box'>
       <h2 className='about__title'>Sobre mí</h2>
-      <p>!Hola¡ Soy Ismael, desde el instituto tuve curiosidad sobre la informática. Decidí orientar mi formación hacia esta rama. Cursé el ciclo superior de desarrollo de aplicaciones web y complemeté mi formación con diferentes cursos, los cuales me sirvieron para conseguir mi primer trabajo.</p>
+      <p>!Hola¡ Soy Ismael, desde el instituto tuve curiosidad sobre la informática. Decidí orientar mi formación hacia esta rama. Cursé el ciclo superior de desarrollo de aplicaciones web y complementé mi formación con diferentes cursos, los cuales me sirvieron para conseguir mi primer trabajo.</p>
 
       <p>Me considero una persona responsable, organizada, asertiva, respetuosa y productiva con ganas de seguir aprendiendo acerca del mundo de la programación.</p>
 
