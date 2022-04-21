1871a17014507c1e718a584ba9aef7062c798ae5
Ismael Cobo <midismael@gmail.com>
2022-04-21 12:40:48 +0200

    feature- add button to see the cv

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,3 +1,7 @@
+import { AiOutlineDownload } from 'react-icons/ai'
+
+import cv from '../../assets/pdf/cv_ismael_cobo.pdf'
+
 import './Hero.css'
 
 
@@ -8,6 +12,7 @@ export const Hero = () => {
                 <h3 className='hero__title'>Hola 👋🏼, soy</h3>
                 <h2 className='hero__name'>Ismael Cobo</h2>
                 <h2 className='hero__title'>Desarrollador Frontend</h2>
+                <a className='hero__cv' href={cv}>Currículum <AiOutlineDownload /></a>
                 {/* <p className='hero__desc'>Desarrollador JavaScript con 10+ años de experiencia. Actualmente formador y divulgador en tecnologías web a través de YouTube, dónde ya somos más de 60,000!</p> */}
             </div>
         </section>
