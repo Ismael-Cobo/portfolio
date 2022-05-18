9ec7e437d6cbfabc84832db427ead3b6d2bb202c
Ismael Cobo <midismael@gmail.com>
2022-05-18 14:13:43 +0200

    feat: new line in about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,7 +7,7 @@ export const About = () => {
       <p>¡Hola! Soy Ismael, desde el instituto tuve curiosidad sobre la informática. Decidí orientar mi formación hacia esta rama. Cursé el ciclo superior de desarrollo de aplicaciones web y complementé mi formación con diferentes cursos, los cuales me sirvieron para conseguir mi primer trabajo.</p>
 
       <p>Me considero una persona responsable, organizada, asertiva, respetuosa y productiva con ganas de seguir aprendiendo acerca del mundo de la programación.</p>
-
+      <p>Amante de redux.</p>
     </div>
   )
 }
